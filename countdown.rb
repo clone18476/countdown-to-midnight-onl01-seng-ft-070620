6b5d3ad956fd006e42e21979a4ea6705dac65dff
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-11 04:45:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,35 +1,44 @@
-
-# def countdown(seconds_left)
- # while number > 0
-  #  puts "#{seconds_left} SECONDS(S)!" 
-   # seconds_left -= 1
-#  end
- # "HAPPY NEW YEAR!"
-# end
-
-
-# def countdown_with_sleep(seconds_left)
-  #  until seconds_left < 1  
-  #  puts "#{seconds_left} SECONDS(S)!" 
-   # seconds_left -= 1
-    #sleep 5
-  #end
-  #"HAPPY NEW YEAR!"
-# end
-
-def countdown(number)
-  while number > 0 do 
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
-
 def countdown
-  def countdown_with_sleep(number)
-    while number > 0 do 
-    puts "#{number} SECOND(S)!"
-    sleep 1
-    number -= 1  
-  end
+  number = 10 
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep 1
+  
 end
+
+print "HAPPY NEW YEAR!"
+coutdown 
+end
+
+
+def countdown_with_sleep
+  number = 10
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep 1
+
+end
+
+print "HAPPY NEW YEAR!"
+countdown_with_sleep
+end
+
+countdown_with_sleep
+# def countdown(number)
+  #while number > 0 do 
+   # puts "#{number} SECOND(S)!"
+  #  number -= 1
+#  end
+#  "HAPPY NEW YEAR!"
+#end
+
+#def countdown
+#  def countdown_with_sleep(number)
+#    while number > 0 do 
+#    puts "#{number} SECOND(S)!"
+#    sleep 1
+#    number -= 1  
+#  end
+#end
