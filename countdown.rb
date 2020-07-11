8d51711ddf71c7b582aefc3b175a121825341c55
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-11 04:52:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,25 +1,47 @@
-def countdown
-  number = 10 
+#def countdown
+ # number = 10 
+#  while number > 0
+#    puts "#{number} SECOND(S)!"
+#    number -= 1
+#    sleep 1
+#  end
+#print "HAPPY NEW YEAR!"
+#end 
+
+#countdown
+#end 
+#
+#def countdown_with_sleep
+#  number = 10
+#  while number > 0
+#    puts "#{number} SECOND(S)!"
+#    number -= 1
+#    sleep 1
+#  end
+#print "HAPPY NEW YEAR!"
+#end#
+#
+#countdown_with_sleep
+#end
+
+def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
+    sleep(1)
     number -= 1
-    sleep 1
   end
-print "HAPPY NEW YEAR!"
-end 
-
-countdown
-end 
-
-def countdown_with_sleep
-  number = 10
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
-    sleep 1
-  end
-print "HAPPY NEW YEAR!"
+  # puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
+  
 end
 
-countdown_with_sleep
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  # puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
+  
 end
